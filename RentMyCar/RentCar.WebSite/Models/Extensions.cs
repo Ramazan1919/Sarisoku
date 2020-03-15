66ca53077ffd90bcbd78b96c95b4bd0403438492
osmansokuoglu <osman.sokuoglu@gmail.com>
2020-03-15 13:40:38 +0300

    RentCar > Rezervasyon yönetimi lookup liste eklendi

---
+++ RentMyCar/RentCar.WebSite/Models/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using BusınessLayer.Util;
+using System.Linq;
+using DataEntity;
+
+namespace RentCar.WebSite.Models
+{
+    public static class Extensions
+    {
+        public static List<SelectListItem> ToItems(this List<LookupItem> lookupList, int selectedValue, bool defaultOption = true)
+        {
+            var result = new List<SelectListItem>();
+            if (defaultOption) { result.Add(new SelectListItem() { Value = "", Text = "Seçiniz" }); }
+
+            if (lookupList.HasElement())
+            {
+                result.AddRange(lookupList.Select(i => new SelectListItem() { Value = i.ID.ToString(), Text = i.Name, Selected = i.ID == selectedValue }));
+            }
+            return result;
+        }
+    }
+}
