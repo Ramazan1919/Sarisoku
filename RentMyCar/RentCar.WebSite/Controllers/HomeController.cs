b74d930aa6b555333dadc543e236fd12f3bf0685
osmansokuoglu <osman.sokuoglu@gmail.com>
2020-03-18 22:24:43 +0300

    SearchCar notes added

--- RentMyCar/RentCar.WebSite/Controllers/HomeController.cs
+++ RentMyCar/RentCar.WebSite/Controllers/HomeController.cs
@@ -38,36 +38,54 @@ namespace RentACar.Controllers
 
         public ActionResult SearchCar(Rezervasyon model)
         {
-
             if (ModelState.IsValid)
             {
+                var carlist = carManager.List(x=>x.IsActive);
+
+                var reservationList = rezervationManager.List(x => x.Status == ReservationsStatus.Active && ((x.AlisTarihi < model.AlisTarihi && x.IadeTarihi > model.AlisTarihi)
+                                        || (x.AlisTarihi > model.AlisTarihi && x.AlisTarihi >= model.IadeTarihi)));
+
+                //Şuana kadar elimizde aktif araç listesi ve verilen tarihlerde yapılmış rezervasyonlar var
+                //Araçları alış yeri / İade yerine göre filtreleyeceksen bunu rezervasyon üzerinden değil ara. üzerinden yapman lazım
+                //Rezervasyon bir araca yapılmış bir işlem oluş X aracını şu tarihte şurdan alacağım şu tarihte şuraya bırakacağım bilgisini içerir
+                //Şimdi eğer adam ben aracı çorumdan alacağım diyorsa o zaman lokasyon bazlı araç listesi olması lazım 
+                //Yani çorumda şuan şu şu şu araçlar var diyeceğimiz bir datayı tutmak lazım. Bunu da şöyle yapabilriiz bence
+                //Bir aracın aynı anda bulanabileceği bir lokasyon olabilir değil mi
+                //Dolaysıyla Car modeli üzerine "LocationID" isminde bir özellik ekleyelim. Aracı oluştururken bunu sçtiririz adama
+                //Daha sonra her rezervasyon tamamlandığında aracın LocationID bilgisi rezervasyonun iade yeri ile güncellenir
+
+                //Veya ikinci alternatif bir yol;
+                //Aracın en son yaptığı rezervasyonu buluruz bu rezervasyondaki iade yeri aracın şuankki yeri olur
+                //Peki araç henüz hiç rezerve edilmezse ilk lokasyonu nere olacak
+                //Bunun içinde araç oluşturulurken ekranda bi konum seçtirip bu konumla bi tane default rezervasyon oluştururuz dolaysıyla aracın
+                //şuanki konumu o rezervasyoındaki iade yeri olur
+
+                //Bu arada araçları sadece biniş yerine göre filtrelemeyebiliriz, iade yerine göre filtrelemenin mantığı yok
+                //Ben aracı çorumdan aldım, site sahibinin bana seçtirdiği istediğim lokasyona bırakabiliriz bunda bi kısıt yok
+
+                //var list = rezervationManager.List(x => x.AlisTarihi <= model.AlisTarihi && x.IadeTarihi >= model.IadeTarihi).Where(
+                //    x => x.AlisYeri == model.AlisYeri && x.İadeYeri == model.İadeYeri);
 
 
-                var carlist = carManager.List();
-                
-                var list = rezervationManager.List(x => x.AlisTarihi <= model.AlisTarihi && x.IadeTarihi >= model.IadeTarihi).Where(
-                    x => x.AlisYeri == model.AlisYeri && x.İadeYeri==model.İadeYeri);
+                //CarID ye göre de filtremelenin bir mantığı yok, eğer araç için filtrleme yapacaksan markasına (Hyundai, Renault), 
+                //modeline (Clio, Accent, Era), Vites Tipine (otomatik, manuel) göre filtreletmek daha mantıklı
+                //Bunları düşünerek arama modelini tekrar düşün üzerinde konuşalım.
 
-                var rezerv=carlist.Where(c => c.Id == model.CarID).ToList();
-                var norezerv= carlist.Where(c => c.Id != model.CarID).ToList();
+                //benim sana ilk ipucum "Rezervasyon model" yerine bir search model oluşturman!! 
+                //Şimdilik aşağıdaki kısımları commentledim sen düşün beraber konuşalım sonra
 
-                if (norezerv == null)
-                {
-                    ViewBag.Nocar = "Belitilen aralıklarda uygun araba bulunamadı.Tekrar Deneyin..";
-                    return View("ListOfCars", norezerv);
-                }
-                else
-                {
+                //var rezerv = carlist.Where(c => c.Id == model.CarID).ToList();
+                //var norezerv = carlist.Where(c => c.Id != model.CarID).ToList();
 
-                    return View("ListOfCars",norezerv);
-                }
-               
-                    
+                //if (norezerv == null)
+                //{
+                //    ViewBag.Nocar = "Belitilen aralıklarda uygun araba bulunamadı.Tekrar Deneyin..";
+                //}
+                //return View("ListOfCars", norezerv);
             }
+
             ViewBag.Nocar = "Belitilen aralıklarda uygun araba bulunamadı.Tekrar Deneyin..";
             return RedirectToAction("ListOfCars", "Home");
-
         }
-
     }
 }
